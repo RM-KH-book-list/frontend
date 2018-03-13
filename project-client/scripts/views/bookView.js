2b8bb6af596ec2f5e0fd4d6aa185dc6a64bbdc7f
Keli Hansen <keli.p.hansen@gmail.com>
2018-03-13 14:52:06 -0700

    style list view

--- project-client/scripts/views/bookView.js
+++ project-client/scripts/views/bookView.js
@@ -8,8 +8,8 @@
 
     bookView.initIndexPage = () => {
         const template = Handlebars.compile($('#book-template').html());
-        Book.all.forEach(data => $('#booklist ul').append(template(data)));
-        $('#booklist').append(Book.all.length, ' total books');
+        Book.all.forEach(data => $('#booklist').append(template(data)));
+        // $('#booklist').append(Book.all.length, ' total books');
     };
 
     module.bookView = bookView;
